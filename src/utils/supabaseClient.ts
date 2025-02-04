56695b4236f846c4813185e07158889e920f37c9
Cesar Aguilar <Blueavian9@gmail.com>
2025-02-04 13:57:05 -0800

    updated dotenv

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,9 +1,13 @@
-import { createClient } from "@supabase/supabase-js"; // ✅ Correct import
+import {createClient }  from "@supabase/supabase-js"; // ✅ Correct import
 
+
+ServerRouter(async (req: Request) => {
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("Missing Supabase environment variables!");
-}
+
+// Do server-side stuff here
+}})
