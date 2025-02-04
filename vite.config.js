ae9bbd86143c0b32d0a6a02c1ffa3255817e00ab
Cesar Aguilar <Blueavian9@gmail.com>
2025-02-03 20:51:47 -0800

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    strictPort: true, // Ensures Vite doesn't switch to another port
+    open: true, // Automatically opens the browser when the server starts
   },
+  envPrefix: "VITE_", // Ensures Vite recognizes env variables with "VITE_" prefix
 });
