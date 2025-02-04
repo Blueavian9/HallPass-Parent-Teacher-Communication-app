2b3b4e739dc041b0931156a06fcb33e6b1dc63f8
Cesar Aguilar <Blueavian9@gmail.com>
2025-02-04 14:21:22 -0800

    updated index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-// import AppRoutes from "./routes.jsx";
+import AppRoutes from "./routes.jsx";
 import App from "./App.jsx"; // Temporarily removing AppRoutes
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- src/utils/supabaseClient.tsx
+++ src/utils/supabaseClient.tsx
@@ -1,13 +1,10 @@
-import {createClient }  from "@supabase/supabase-js"; // ✅ Correct import
-
-
-ServerRouter(async (req: Request) => {
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("Missing Supabase environment variables!");
+}
 
-// Do server-side stuff here
-}})
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
