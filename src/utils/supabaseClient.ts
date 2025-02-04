d124910d72885ff12ab2e74d29a393eb7339959f
Cesar Aguilar <Blueavian9@gmail.com>
2025-02-03 20:25:27 -0800

    updated supabaseClient.ts

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,4 +1,4 @@
-import { supabase } from "./utils/supabaseClient"; 
+import { supabase } from "./utils/supabaseClient.ts";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
