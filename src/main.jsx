b6b85ff5560c43d0594af906897e2e48280b28d2
Cesar Aguilar <Blueavian9@gmail.com>
2025-02-03 20:42:52 -0800

    updated App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes.jsx";
-import App from "./App.jsx";
+// import AppRoutes from "./routes.jsx";
+import App from "./App.jsx"; // Temporarily removing AppRoutes
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AppRoutes />
+    <App />
   </BrowserRouter>
 );
