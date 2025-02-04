dff83bd35837718a59f5dbcffba1a8b48f8e237f
Cesar Aguilar <Blueavian9@gmail.com>
2025-02-04 11:24:02 -0800

    main

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js"; // ✅ Correct import
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+
+const supabaseUrl = 'https://uvbefnnzcblmhzmsqruj.supabase.co'
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); // ✅ No conflicts
