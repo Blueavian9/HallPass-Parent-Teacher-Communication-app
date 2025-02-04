7306a4ff57a71c6f5126cfe9dab75f3e09b007bb
Cesar Aguilar <Blueavian9@gmail.com>
2025-02-03 21:12:53 -0800

    updated vite.config.js

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,12 @@
-import { Route, Routes } from "react-router-dom";
-import App from "./App.jsx";
+import { Routes, Route, Navigate } from "react-router-dom";
+import App from "./App";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<App />} />
+      {/* Redirect any unknown paths to home */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // Change this if deploying to Github pages
   plugins: [react()],
   server: {
     port: 3000,
