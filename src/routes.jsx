50d9c94bfac4a0843b21a6b3fc4e209f79d699e6
Cesar Aguilar <Blueavian9@gmail.com>
2025-02-11 13:46:52 -0800

    updated routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,10 +3,16 @@ import App from "./App";
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<App />} />
-      {/* Redirect any unknown paths to home */}
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="/" element={<App />} />
+    //   {/* Redirect any unknown paths to home */}
+    //   <Route path="*" element={<Navigate to="/" replace />} />
+    // </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
