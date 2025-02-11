a0cbdcc3bd09921abfb37e2b2b9db2543760dae2
Cesar Aguilar <Blueavian9@gmail.com>
2025-02-11 14:53:49 -0800

    updated database.types.ts

---
+++ database.types.ts
@@ -0,0 +1,14 @@
+{
+    "compilerOptions": {}, 
+    "files": [
+        "core.ts",
+        "sys.ts",
+        "types.ts", 
+        "scanner.ts", 
+        "parser.ts", 
+        "utilities", 
+        "blinder.ts",
+        "checker.ts",
+        "tsc.ts"
+    ]
+}
