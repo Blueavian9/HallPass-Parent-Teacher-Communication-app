a0efda8dc86072dbe2127dc4fa4cf05b08a95ed3
Cesar Aguilar <Blueavian9@gmail.com>
2025-02-11 09:04:44 -0800

    Updated Home.tsx path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { supabase } from "./utils/supabaseClient.ts";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./utils/Home.tsx";
+// import Home from "./utils/Home.tsx";
 // import axios from "axios";
 
 export default function App() {
