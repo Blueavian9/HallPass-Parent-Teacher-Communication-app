2f19921771b5183e46f788d84dc66c30380d143d
Cesar Aguilar <Blueavian9@gmail.com>
2025-02-13 13:39:42 -0800

    updated Home.tsx

--- HallPass-Parent-Teacher-Communication-app/src/Home.tsx
+++ HallPass-Parent-Teacher-Communication-app/src/Home.tsx
@@ -1,20 +1,28 @@
 import React from "react";
-import { Message } from "./database.types.ts";
+import { Message } from "./database.types";
 
 interface HomeProps {
   data: Message[];
 }
 
 const Home: React.FC<HomeProps> = ({ data }) => {
-  return (<div>
-    <h1>Welcome to HallPass Homepage App</h1>
-    <ul>
-      {data.map((message) => (
-        <li key={message.id}>{message.content}</li>
-      ))}
-    </ul>
-  </div>
-  ); 
+  return (
+    <div>
+      <h1>Welcome to HallPass Homepage App</h1>
+      {data.length > 0 ? (
+        <ul>
+          {data.map((message) => (
+            <li key={message.id}>
+              <strong>Message:</strong> {message.content} <br />
+              <small>Created At: {new Date(message.created_at).toLocaleString()}</small>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No messages available.</p>
+      )}
+    </div>
+  );
 };
 
 export default Home;
